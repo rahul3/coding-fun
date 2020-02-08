680f6e520dd8f33aa9e26ad1231dccb899285c90
Rahul Padmanabhan <rahul3@mail.usf.edu>
2020-02-07 23:08:45 -0500

    addition logic, works for base case, more work needed

--- astar/astar.py
+++ astar/astar.py
@@ -1,15 +1,17 @@
 import copy
 
-input_map = [[False, False, False, False],
+def get_input_map():
+    input_map = [[False, False, False, False],
             [True, True, False, True],
             [False, False, False, False],
             [False, False, False, False]]
+    return input_map
 
 class Node(object):
     
     def __init__(self, input_node, target_node):
         """Initalizing"""
-        print(f"Input node is {input_node}, target node is {target_node}")
+#         print(f"Input node is {input_node}, target node is {target_node}")
         self.coords = input_node
         self.target_coords = target_node
         self.score = self.heuristic(input_node, target_node)
@@ -33,7 +35,7 @@ class Node(object):
         return (self.coords, self.target_coords)
 
     def __hash__(self):
-        return hash(self.__key__())
+        return hash(self.__key())
     
     def __getitem__(self, indx):
         p = copy.copy(self)
@@ -53,7 +55,7 @@ class Node(object):
             return False
         
         
-    def get_next_steps(self, input_map: "list of lists") -> "list of Node objects":
+    def get_next_steps(self, coords, input_map: "list of lists") -> "list of Node objects":
         """
             Gets the value of possible options for the next step
         """
@@ -61,23 +63,31 @@ class Node(object):
         final_options = []
         left, right, top, bottom = "None"
         rows, cols = self.get_dimensions(input_map)
-        left = (self.coords[0]-1, self.coords[1]) if self.coords[0]-1 >= 0 else "None"
-        right = (self.coords[0]+1, self.coords[1]) if self.coords[0]+1 <= cols else "None"
-        top = (self.coords[0], self.coords[1]-1) if self.coords[1]-1 >= 0 else "None"
-        bottom = (self.coords[0], self.coords[1]+1) if self.coords[1]+1 <= rows else "None"
+        left = (coords[0], coords[1]-1) if coords[1]-1 >= 0 else "None"
+        right = (coords[0], coords[1]+1) if coords[1]+1 <= cols else "None"
+        top = (coords[0]-1, coords[1]) if coords[0]-1 >= 0 else "None"
+        bottom = (coords[0]+1, coords[1]) if coords[0]+1 <= rows else "None"
         
+#         print(f"Current Coordinates {coords}")
+#         print(f"left: {left}\t right: {right}\t top: {top}\t bottom: {bottom}")
         options = set([left, right, top, bottom])
         try:
             options.remove("None")
         except KeyError:
             pass
+#         print(f"Options are {options}")
+        
+        if not options:
+            return []
         
         for option in options:
-            if option is not None:
+            if option is not "None":
                 final_options.append(Node(option, self.get_target()))
                 
         # Over writing options
         options = [x.coords for x in final_options]
+        if final_options:
+            final_options = [node for node in final_options if node.is_valid(get_input_map())]
         
         return final_options
     
@@ -113,6 +123,7 @@ class Node(object):
     def get_hscore(self):
         return self.score
 
+
 class AStar(object):
     
     def __init__(self, input_map=None, initial=None, target=None):
@@ -133,42 +144,71 @@ class AStar(object):
             
         self.visited_nodes = []  # all coordinates visited
         self.path = [] # traversal path
+        self.cameFrom = {}
+        
+        self.iterations = 0
+        
+    def reconstruct_path(cameFrom: dict, current) -> list:
+        total_path = current
+        coordinates = [x.coords for x in cameFrom.keys()]
+        while current in coordinates:
+            current = cameFrom[current].coords
+            total_path.insert(0, current)
+            
+        return total_path
+        
         
     def astar(self, start_node=None, end_node=None):
         """ A Star algorithm"""
-        print(self.target)
-        self.start = start_node if start_node is not None else self.start
+        self.start = self.start if start_node is None else start_node
+        print(f"Current node is: {self.start}")
         self.end = self.target if end_node is None else end_node
         self.current = Node(self.start, self.end)
-        self.iterations = 0 # testing code
         self.visited_nodes.append(self.current)
         
+        min_score = 0
+        flag = 0
+        
+        self.cameFrom[self.current] = self.current.score
+        print(f"Came from dict is {[x.coords for x in self.cameFrom.keys()]}")
+        """node immediately preceding it on the cheapest path from start to n currently known."""
+        self.cameFrom_nodes = [x for x in self.cameFrom.keys()]
+        
         if self.start == self.end:
-            print(f"Destination Reached {end_node}")
-        else:
+            flag = 1
+            print(f"Destination Reached")
+
+        elif flag == 0:
             while self.visited_nodes:
-                self.iterations += 1 # testing code
-                if self.iterations >= 4: # testing code
-                    break # testing code
+#                 self.iterations += 1 # testing code
+#                 if self.iterations >= 4: # testing code
+#                     break # testing code
                     
-                for node in self.visited_nodes:
-                    if self.current.is_valid(input_map):
-                        next_steps = self.current.get_next_steps(input_map)
+                for node in self.cameFrom_nodes:
+                    if node.is_valid(self.input_map):
+                        next_steps = node.get_next_steps(self.current.coords, self.input_map)
                         
                         if next_steps:
-                            for n_node in next_steps:
-                                if n_node not in self.visited_nodes and n_node.is_valid(input_map):
-                                    self.visited_nodes.append(n_node)
-                                    break;
-                            min_score = min([x.score for x in self.visited_nodes])
-                            next_node = [x for x in self.visited_nodes if x.score==min_score]
-                            if next_node:
-                                print(f"Next node is {next_node}")
-                                
-                                
-                            ### TO DO
-
+                            steps_scores = [x.score for x in next_steps if x not in self.visited_nodes]
+                            if not steps_scores:
+                                return 0
+                            print(f"steps_scores is {steps_scores}")
+                            min_score = min(steps_scores)
+                            
+                        next_nodes = [x for x in next_steps if x.score==min_score and x not in self.visited_nodes]
+                        print(f"Next options are {[x.coords for x in next_nodes]}")
+                            
+                        if next_nodes:
+                            print(f"Next node is {next_nodes[0].coords}")
+                            self.cameFrom[next_nodes[0]] = next_nodes[0].score
+                            print(f"Visted Nodes so far is {[x.coords for x in self.visited_nodes]}")
+                            print("**********ITERATION END************\n")
+                            if self.start == self.end:
+                                break
+                            self.astar(next_nodes[0].coords, self.end)
+                
         return [x.coords for x in self.visited_nodes]
 
-a = AStar()
-a.astar()
+if __name__ == "__main__":
+    a = AStar()
+    a.astar()
